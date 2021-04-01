2ad1c76b73e08c9b96fbc7ee34bae27d5d558344
Afesss <afesdaller@gmail.com>
2021-04-01 20:48:05 +0300

    fsm

---
+++ Assets/Scripts/FSM/FSM.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+internal class FSM
+{
+    internal State CurrentState { get; private set; }
+    internal State PreviousState { get; private set; }
+    private Type currentStateType;
+    internal void SetStartState(State startState)
+    {
+        CurrentState = startState;
+        CurrentState.Enter();
+        currentStateType = CurrentState.GetType();
+    }
+    internal void ChangeState(State newState)
+    {
+        if (newState == CurrentState)
+            return;
+
+        if (CurrentState != null)
+        {
+            PreviousState = CurrentState;
+            CurrentState.Exit();
+        }
+
+        CurrentState = newState;
+        CurrentState.Enter();
+        currentStateType = CurrentState.GetType();
+    }
+
+    
+    private Dictionary<Type, List<Func<bool>>> transitions = new Dictionary<Type, List<Func<bool>>>();
+
+    private Type stateType;
+
+    internal void AddTransition(State from,State to, Func<bool> transitionTo)
+    {
+        stateType = from.GetType();
+        if (!this.transitions.TryGetValue(stateType, out var transitions)) 
+        {
+            transitions = new List<Func<bool>>();
+            this.transitions.Add(stateType, transitions);
+        }
+        transitions.Add(transitionTo);
+        stateType = to.GetType();
+        if (!this.transitions.ContainsKey(stateType))
+            this.transitions.Add(stateType, new List<Func<bool>>());
+    }
+    private int iterationNum;
+    internal void TransitionsUpdate()
+    {
+        for(iterationNum = 0; iterationNum < transitions[currentStateType].Count; iterationNum++)
+        {
+            if (transitions[currentStateType][iterationNum].Invoke())
+                return;
+        }
+    }
+    private class Transition
+    {
+        internal Func<bool> Condition { get; }
+        internal State To { get; }
+        internal Transition(State to,Func<bool> condition)
+        {
+            Condition = condition;
+            To = to;
+        }
+    }
+}
+
---
+++ Assets/Scripts/FSM/State.cs
@@ -0,0 +1,9 @@
+
+internal abstract class State
+{
+    internal virtual void Enter() { }
+    internal virtual void Exit() { }
+    internal virtual void PhysicsUpdate() { }
+    internal virtual void Update() { }
+    
+}
