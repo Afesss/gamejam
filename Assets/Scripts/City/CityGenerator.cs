9591c1f8ca0936bfbdbcd82aff719390eddc5e3e
Erzyukov <erzyukov@gmail.com>
2021-04-02 14:47:50 +1000

    Генератор города
    Реализовал генератор города, компоненты для повреждения дома и визуальное отображение состояния дома.

---
+++ Assets/Scripts/City/CityGenerator.cs
@@ -0,0 +1,83 @@
+using System;
+using UnityEngine;
+
+/// <summary>
+/// Генератор города
+/// </summary>
+public class CityGenerator : MonoBehaviour
+{
+    [Tooltip("Количество колонок для домов")]
+    [SerializeField]
+    private int cityColsCount;
+
+    [Tooltip("Количество линий для домов")]
+    [SerializeField]
+    private int cityRowsCount;
+
+    [Tooltip("Размер ячейки на которой стоит дом")]
+    [SerializeField]
+    private Vector2 CellSize;
+
+    [Tooltip("Объекты домов")]
+    [SerializeField]
+    private GameObject[] houseObjects;
+
+    [Tooltip("Процентное соотношение домов")]
+    [SerializeField]
+    private int[] houseRates;
+
+    private int[] remainHousesToPlace;
+    private int freeHouseTypesCount;
+
+
+    private void Awake()
+    {
+        InitHousesCounts();
+
+//        foreach (var value in remainHousesToPlace)
+//            Debug.Log(value);
+
+        for (var x = 0; x < cityColsCount; x++)
+        {
+            for (var z = 0; z < cityRowsCount; z++)
+            {
+                InstantiateHouse(x, z, GetRandomFreeHouseIndex());
+            }
+        }
+    }
+
+    private int GetRandomFreeHouseIndex()
+    {
+        Array.Sort(remainHousesToPlace);
+        Array.Reverse(remainHousesToPlace);
+
+        var index = UnityEngine.Random.Range(0, freeHouseTypesCount);
+        remainHousesToPlace[index] --;
+        if (remainHousesToPlace[index] <= 0)
+            freeHouseTypesCount--;
+
+        return index;
+    }
+
+    private void InitHousesCounts()
+    {
+        freeHouseTypesCount = houseObjects.Length;
+
+        var count = cityColsCount * cityRowsCount;
+        remainHousesToPlace = new int[houseObjects.Length];
+        var sum = 0;
+        for(var i = 0; i < houseObjects.Length - 1; i++)
+        {
+            remainHousesToPlace[i] = (int) ((float) houseRates[i] * count / 100);
+            sum += remainHousesToPlace[i];
+        }
+        remainHousesToPlace[houseObjects.Length - 1] = count - sum;
+    }
+
+    private void InstantiateHouse(int col, int row, int index)
+    {
+        var position = new Vector3(col * CellSize.x, 0, row * CellSize.y);
+        GameObject.Instantiate(houseObjects[index], position, Quaternion.identity, transform);
+    }
+
+}
